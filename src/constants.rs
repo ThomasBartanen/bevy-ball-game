ccf590b9282df1b5702a8d40ccd86fa39a845c63
Thomas Bartanen <Bartanen.Production@gmail.com>
2023-09-25 22:56:53 -0700

    Update constants.rs

--- src/constants.rs
+++ src/constants.rs
@@ -8,6 +8,7 @@ pub const MAX_ENEMIES: usize = 15;
 pub const ENEMY_SPEED: f32 = 200.0;
 pub const ENEMY_SIZE: f32 = 64.0;
 pub const ENEMY_SPAWN_TIME: f32 = 10.0;
+pub const ENEMY_KILL_SCORE: u32 = 2;
 
 // === Stars ===
 pub const NUMBER_OF_STARS: usize = 10;
@@ -16,7 +17,10 @@ pub const STAR_SCORE: u32 = 1;
 pub const STAR_SPAWN_TIME: f32 = 1.0;
 
 // === Bombs ===
-pub const BOMB_COST: u32 = 2;
+pub const BOMB_COST: f32 = 10.0;
 pub const BOMB_DET_TIME: f32 = 0.75;
-pub const BOMB_DET_RADIUS: f32 = 5.0;
+pub const BOMB_COOLDOWN_TIME: f32 = 1.2;
+
+// === Explosions ===
+pub const EXPLOSION_RADIUS: f32 = 64.0;
 pub const EXPLOSION_VISIBILITY_TIME: f32 = 0.2;
