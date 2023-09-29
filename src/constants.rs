5335229cdc24f459314a7e2d87f520791204b4ca
Thomas Bartanen <Bartanen.Production@gmail.com>
2023-09-28 21:29:58 -0700

    Update constants.rs
    Make the game a bit more difficult

--- src/constants.rs
+++ src/constants.rs
@@ -4,10 +4,10 @@ pub const PLAYER_SIZE: f32 = 64.0; // player SPRITE size in pixels
 
 // === Enemies ===
 pub const NUMBER_OF_ENEMIES: usize = 4;
-pub const MAX_ENEMIES: usize = 15;
-pub const ENEMY_SPEED: f32 = 200.0;
+pub const MAX_ENEMIES: usize = 20;
+pub const ENEMY_SPEED: f32 = 300.0;
 pub const ENEMY_SIZE: f32 = 64.0;
-pub const ENEMY_SPAWN_TIME: f32 = 10.0;
+pub const ENEMY_SPAWN_TIME: f32 = 5.0;
 pub const ENEMY_KILL_SCORE: u32 = 2;
 
 // === Stars ===
@@ -22,7 +22,8 @@ pub const BOMB_COOLDOWN_TIME: f32 = 1.2;
 
 // === Explosions ===
 pub const EXPLOSION_RADIUS: f32 = 64.0;
-pub const EXPLOSION_VISIBILITY_TIME: f32 = 0.2;pub const EXPLOSION_VISIBILITY_TIME: f32 = 0.2;
+pub const EXPLOSION_VISIBILITY_TIME: f32 = 0.2;
+
 // === CURRENCY ===
 pub const BOMB_COST: f32 = 10.0;
 pub const CURRENCY_PER_KILL: f32 = 5.0;
